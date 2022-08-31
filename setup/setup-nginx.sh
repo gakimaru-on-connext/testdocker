6a6c495402c218283174002ef6b32960cf406186
Itagaki Mamoru <67382040+gakimaru-on-connext@users.noreply.github.com>
2022-09-01 08:26:54 +0900

    nginx のセットアップを追加

---
+++ setup/setup-nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo Setup NginX
+
+dnf -y install nginx
+
+systemctl start nginx
+systemctl enable nginx
+systemctl status nginx
+
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=https
+firewall-cmd --reload
+
+# client
+# $ curl http://192.168.56.10
